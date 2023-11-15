4fa6fa129b9dc01e079253936efe437344cb5f3e
nejsemTonda <vaclav@krnak.cz>
2023-11-15 15:50:21 +0100

    sloved bug for ambiguous from and to

--- code/api.py
+++ code/api.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from connection import Connection, Transport
-   
-
+ 
+ambi_error = "Zadání není jednoznačné, vyberte prosím z nabízeného seznamu."
+unknown_error = "Takové místo neznáme."
 
 def get_connections(f: str, t: str, means="vlakyautobusymhdvse"):
     """
@@ -14,11 +15,30 @@ def get_connections(f: str, t: str, means="vlakyautobusymhdvse"):
     Returns:
         list of Connections classes
     """
+    possible_labels = [-1, 1, 2, 301003]
+    from_label = -1
+    to_label = -1
 
-    url = f"https://idos.idnes.cz/{means}/spojeni/vysledky/?f={f}&t={t}"
-    r = requests.get(url)
+    while(True):
+        url = f"https://idos.idnes.cz/{means}/spojeni/vysledky/?f={f}&fc={from_label}&t={t}&tc={to_label}"
+        print(url)
 
-    soup = BeautifulSoup(r.text, "html.parser")
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "html.parser")
+        to_box = soup.find('label', {'for': 'To'})
+        from_box = soup.find('label', {'for': 'From'})
+
+        if to_box is None or from_box == None:
+            break
+
+        try:
+            if ambi_error in from_box.text or unknown_error in from_box.text:
+                from_label = possible_labels[possible_labels.index(from_label)+1]
+
+            if ambi_error in to_box.text or unknown_error in to_box.text:
+                to_label = possible_labels[possible_labels.index(to_label)+1]
+        except IndexError:
+            raise ValueError(f"Zadání {f} -- > {to} bylo nejednoznačné")
 
     connection_boxes = soup.find_all(lambda tag: tag.has_attr('id') and tag['id'].startswith('connectionBox'))
     
