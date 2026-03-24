3658b85166bd67229f44b5cdfd47647097c94d31
Václav Krňák <vaclav@krnak.cz>
2026-03-24 23:40:15 +0100

    run playwirght only when needed

--- code/api.py
+++ code/api.py
@@ -9,6 +9,31 @@ unknown_error = "Takové místo neznáme."
 means_id = {"tram": 300, "bus": 301, "metro": 302, "vlak": 315, "all": "150,151,152,153,154,155,156,200,201,202,300,301,302,303,304,305,306,307,308,309,310,311,312,314,315,317,318,319"}
 
 
+def _fetch(url, page=None):
+    if page is not None:
+        page.goto(url)
+        try:
+            page.wait_for_selector("[id^='connectionBox']", timeout=8000)
+        except Exception:
+            pass
+        return page.content()
+    try:
+        return requests.get(url).text
+    except requests.exceptions.ConnectionError:
+        print("Nebylo možné najít spoje, zařízení není přípojeno k internetu")
+        quit(1)
+
+
+def _needs_js(soup):
+    boxes = soup.find_all(lambda tag: tag.has_attr("id") and tag["id"].startswith("connectionBox"))
+    for box in boxes:
+        for ul in box.find_all("ul"):
+            if "stations" in (ul.get("class") or []):
+                if not ul.find("p"):
+                    return True
+    return False
+
+
 def get_connections(f: str, t: str, means="all", con_time=None, arr=False):
     """
     This function send a request to IDOS and finds a connections for give stations
@@ -24,61 +49,62 @@ def get_connections(f: str, t: str, means="all", con_time=None, arr=False):
     possible_labels = [-1, 1, 2, 301003]
     from_label = -1
     to_label = -1
+    page = None
 
-    from playwright.sync_api import sync_playwright
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
+    while True:
+        url = f"https://idos.idnes.cz/vlakyautobusymhdvse/spojeni/vysledky/?f={f}&fc={from_label}&t={t}&tc={to_label}"
 
-        while True:
-            url = f"https://idos.idnes.cz/vlakyautobusymhdvse/spojeni/vysledky/?f={f}&fc={from_label}&t={t}&tc={to_label}"
+        if con_time is not None:
+            url += f"&time={con_time}"
 
-            if con_time is not None:
-                url += f"&time={con_time}"
+        if arr:
+            url += f"&arr=true"
 
-            if arr:
-                url += f"&arr=true"
+        if means != "all":
+            url += "&af=true"
+            tokens = means.split(",")
+            if tokens[0] == "only":
+                ids = list(map(str, [means_id[t] for t in tokens[1:]]))
+                trt = ",".join(ids)
 
-            if means != "all":
-                url += "&af=true"
-                tokens = means.split(",")
-                if tokens[0] == "only":
-                    ids = list(map(str, [means_id[t] for t in tokens[1:]]))
-                    trt = ",".join(ids)
+            if tokens[0] == "exclude":
+                trt = means_id["all"]
+                ids = [means_id[t] for t in tokens[1:]]
+                for i in ids:
+                    trt = trt.replace("," + str(i), "")
 
-                if tokens[0] == "exclude":
-                    trt = means_id["all"]
-                    ids = [means_id[t] for t in tokens[1:]]
-                    for i in ids:
-                        trt = trt.replace("," + str(i), "")
+            url += "&trt=" + trt
 
-                url += "&trt=" + trt
+        r = _fetch(url, page)
+        soup = BeautifulSoup(r, "html.parser")
 
-            page.goto(url)
-            try:
-                page.wait_for_selector("[id^='connectionBox']", timeout=8000)
-            except Exception:
-                pass
-            r = page.content()
-
+        if _needs_js(soup) and page is None:
+            from playwright.sync_api import sync_playwright
+            _pw = sync_playwright().start()
+            _browser = _pw.chromium.launch(headless=True)
+            page = _browser.new_page()
+            r = _fetch(url, page)
             soup = BeautifulSoup(r, "html.parser")
-            to_box = soup.find("label", {"for": "To"})
-            from_box = soup.find("label", {"for": "From"})
 
-            if to_box is None or from_box is None:
-                break
+        to_box = soup.find("label", {"for": "To"})
+        from_box = soup.find("label", {"for": "From"})
 
-            try:
-                if ambi_error in from_box.text or unknown_error in from_box.text:
-                    from_label = possible_labels[possible_labels.index(from_label) + 1]
+        if to_box is None or from_box is None:
+            break
 
-                if ambi_error in to_box.text or unknown_error in to_box.text:
-                    to_label = possible_labels[possible_labels.index(to_label) + 1]
-            except IndexError:
-                print((f"Zadání {f} -- > {t} nebylo jednoznačné, nebo nebylo možné najít zadané místo"))
-                quit(1)
+        try:
+            if ambi_error in from_box.text or unknown_error in from_box.text:
+                from_label = possible_labels[possible_labels.index(from_label) + 1]
 
-        browser.close()
+            if ambi_error in to_box.text or unknown_error in to_box.text:
+                to_label = possible_labels[possible_labels.index(to_label) + 1]
+        except IndexError:
+            print((f"Zadání {f} -- > {t} nebylo jednoznačné, nebo nebylo možné najít zadané místo"))
+            quit(1)
+
+    if page is not None:
+        _browser.close()
+        _pw.stop()
 
     print(url)
     connection_boxes = soup.find_all(lambda tag: tag.has_attr("id") and tag["id"].startswith("connectionBox"))
